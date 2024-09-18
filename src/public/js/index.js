fc6810cd19ac47ec977cd74912b28c89573fcfdd
Matias Fazzito <matyf11@hotmail.com>
2024-09-18 07:30:31 -0300

    Untrack files in .gitignore

--- src/public/js/index.js
+++ src/public/js/index.js
@@ -1,10 +1,7 @@
 const socket = io()
 
 socket.emit("message", "Hola aca estoy")
-
-
 const messageContainer = document.getElementById("messageContainer")
-
 socket.on("message", data => {
     messageContainer.innerHTML = `<p>${data}</p>`
 })
